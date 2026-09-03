b1b9f6a8d379915fa6aa071dadf9ae69ceee217f
Jaime Moreno <jaimemorenosanchez96@gmail.com>
2026-09-03 20:31:20 +0200

    fix(oauth): preserve token URI for credential reconstruction

--- app/backend/web_app.py
+++ app/backend/web_app.py
@@ -141,7 +141,7 @@ def google_callback():
         session['credentials'] = {
             'token': credentials.token,
             'refresh_token': credentials.refresh_token,
-            # 'token_uri': credentials.token_uri,
+            'token_uri': credentials.token_uri,
             'client_id': credentials.client_id,
             'client_secret': credentials.client_secret,
             'scopes': credentials.scopes
